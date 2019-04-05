47de6c01f36732a156fb26bbd5ec80e371ab199d
EwRicklefs <ew.ricklefs@gmail.com>
2019-04-05 12:23:28 -0700

    more

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,33 +2,33 @@
 //TODO: hardcode some example friends into this list.
 var friends = []
 
-$('form').submit(false);
+// $('form').submit(false);
 
-      //submit button functionality
-$('#submit').on('click', ()=> {
-    if($('#name').val() && $('#url').val()) {
-        var newArr = []
-        newArr.push($("#q1").val())
-        newArr.push($("#q2").val())
-        newArr.push($("#q3").val())
-        newArr.push($("#q4").val())
-        newArr.push($("#q5").val())
-        newArr.push($("#q6").val())
-        newArr.push($("#q7").val())
-        newArr.push($("#q8").val())
-        newArr.push($("#q9").val())
-        newArr.push($("#q10").val())
-        var nameVal = $('#name').val()
-        var imgUrl = $('#url').val()
-        var newObj = {
-            name: nameVal,
-            url: imgUrl,
-            vals: newArr
-        }
-        friends.push(newObj)
-    } else {
-        $('#errModal').modal('show')
-    }
-})
+//       //submit button functionality
+// $('#submit').on('click', ()=> {
+//     if($('#name').val() && $('#url').val()) {
+//         var newArr = []
+//         newArr.push($("#q1").val())
+//         newArr.push($("#q2").val())
+//         newArr.push($("#q3").val())
+//         newArr.push($("#q4").val())
+//         newArr.push($("#q5").val())
+//         newArr.push($("#q6").val())
+//         newArr.push($("#q7").val())
+//         newArr.push($("#q8").val())
+//         newArr.push($("#q9").val())
+//         newArr.push($("#q10").val())
+//         var nameVal = $('#name').val()
+//         var imgUrl = $('#url').val()
+//         var newObj = {
+//             name: nameVal,
+//             url: imgUrl,
+//             vals: newArr
+//         }
+//         friends.push(newObj)
+//     } else {
+//         $('#errModal').modal('show')
+//     }
+// })
 
 module.exports = friends;
